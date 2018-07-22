0209e5f6b82472ce38a6b40e6d3583107b90e1df
Badr Youbi Idrissi <badryoubiidrissi@gmail.com>
2018-07-22 15:30:35 +0200

    Restructured app and moved state down

--- src/components/App.js
+++ src/components/App.js
@@ -1,99 +1,21 @@
 import React from "react";
-import {connect} from "react-redux";
-import {addTask} from "../actions";
-import Quadrant from "./Quadrants.js";
-import axios from "axios";
-import {Grid} from "semantic-ui-react";
+import Matrix from "./Matrix";
+import PostItBlock from "./PostItBlock"
+import {categories} from "../constants";
 
-class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = this.defaultValues();
-    }
-
-    defaultValues() {
-        return { 
-            values : {
-                urgImp   : "",
-                nurgImp  : "",
-                urgNimp  : "",
-                nurgNimp : "",
-            } 
-        }
-    }
-
-    componentDidMount() {
-        this.apiTest();
-    }
-
-    apiTest = async () => {
-        var resultat = await axios.get('/api');
-        console.log(resultat);
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-        const name = event.target.name;
-        this.props.addTask(name,this.state.values[name]);
-        this.setState(this.defaultValues);
-    }
-    handleChange = (event) => {
-        const values = {...this.state.values};
-        values[event.target.name] = event.target.value;
-        this.setState({
-            values,
-        });
-    }
+export default class App extends React.Component{
     render(){
         return (
             <div className="app">
-                <Grid>
-                    <Grid.Row>                
-                        <Quadrant 
-                            key="nurgImp"
-                            name="nurgImp"
-                            tasks={this.props.tasks["nurgImp"]}
-                            onSubmit={this.handleSubmit.bind(this)}
-                            onChange={this.handleChange.bind(this)} 
-                            inputValue={this.state.values["nurgImp"]}/>
-                        <Quadrant 
-                            key={"urgImp"}
-                            name={"urgImp"}
-                            tasks={this.props.tasks["urgImp"]}
-                            onSubmit={this.handleSubmit.bind(this)}
-                            onChange={this.handleChange.bind(this)} 
-                            inputValue={this.state.values["urgImp"]}/>
-                    </Grid.Row>
-                    <Grid.Row>    
-                        <Quadrant 
-                            key={"nurgNimp"}
-                            name={"nurgNimp"}
-                            tasks={this.props.tasks["nurgNimp"]}
-                            onSubmit={this.handleSubmit.bind(this)}
-                            onChange={this.handleChange.bind(this)} 
-                            inputValue={this.state.values["nurgNimp"]}/>
-                        <Quadrant 
-                            key={"urgNimp"}
-                            name={"urgNimp"}
-                            tasks={this.props.tasks["urgNimp"]}
-                            onSubmit={this.handleSubmit.bind(this)}
-                            onChange={this.handleChange.bind(this)} 
-                            inputValue={this.state.values["urgNimp"]}/>
-                    </Grid.Row>
-                </Grid>
+                <Matrix tasks={this.props.tasks}/>
+                <div id="PostItBlocks">
+                    <PostItBlock id={categories.N_URGENT_N_IMPORTANT}/>
+                    <PostItBlock id={categories.URGENT_N_IMPORTANT}/>
+                    <PostItBlock id={categories.N_URGENT_IMPORTANT}/>
+                    <PostItBlock id={categories.URGENT_IMPORTANT}/>
+                </div>
             </div>
+            
         );
     }
 }
-
-function mapStateToProps(state){
-    return {
-        tasks : state.tasks
-    }
-}
-
-const mapDispatchToProps = {
-    addTask
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/components/PostItBlock.js
@@ -0,0 +1,49 @@
+import React from "react";
+import {connect} from "react-redux";
+import {addTask} from "../actions";
+import {explName} from "../constants/";
+
+
+class PostItBlock extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.category = props.id;
+        this.state = { 
+            value: ""
+         };
+    }
+    handleSubmit = event => {
+        event.preventDefault();
+        this.props.addTask(this.category,this.state.value);
+        this.setState({value:""});
+    }
+
+    handleChange = event => {
+        this.setState({value : event.target.value})
+    };
+
+    render(){
+        return (
+            <form className="PostItBlock" id={this.category} onSubmit={this.handleSubmit}>
+                <input 
+                    name = {this.props.name}
+                    value = {this.state.value}
+                    onChange={this.handleChange}/>
+            </form>
+        );
+    }
+}
+
+
+function mapStateToProps(state){
+    return {
+        tasks : state.tasks
+    }
+}
+
+const mapDispatchToProps = {
+    addTask
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostItBlock);
