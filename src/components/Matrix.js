7a9760429c63fb9aeb7167c48c345d07ccc785da
Badr Youbi Idrissi <badryoubiidrissi@gmail.com>
2018-07-23 21:17:21 +0900

    Listening to window resize to determine rowHeight

--- src/components/Matrix.js
+++ src/components/Matrix.js
@@ -7,29 +7,31 @@ export default class Matrix extends React.Component{
     constructor(props){
       super(props)
       this.default = {
-        cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }
+        breakpoints: {lg: 1800*0.70, md: 1200*0.7, sm: 900*0.9, xs: 600*0.95},
+        cols: {lg: 12, md: 10, sm: 6, xs: 4, xxs:2}
       }
       this.state = {
         currentBreakpoint : "lg",
         rowHeight : 130
       }
     }
-    componentDidUpdate(prevProps,prevState){
+
+    setRowHeight = () => {
       var node = ReactDOM.findDOMNode(this);
       if (node instanceof HTMLElement){
-        var rowHeight = node.offsetWidth / this.default.cols[this.state.currentBreakpoint]/2;
-        if(prevState.rowHeight !== rowHeight){
-          this.setState({rowHeight});
-        }
+        var rowHeight = node.offsetWidth / this.default.cols[this.state.currentBreakpoint];
+        this.setState({rowHeight});
       } 
     }
 
     componentDidMount(){
-      var node = ReactDOM.findDOMNode(this);
-      if (node instanceof HTMLElement){
-        var rowHeight = node.offsetWidth / this.default.cols[this.state.currentBreakpoint]/2;
-        this.setState({rowHeight});
-      } 
+      window.addEventListener("resize", this.setRowHeight);
+      window.addEventListener("load", this.setRowHeight)
+      this.setRowHeight();
+    }
+
+    componentWillUnmount(){
+      window.removeEventListener("resize", this.setRowHeight);
     }
 
     onBreakPointChange = (breakpoint) => {
@@ -41,13 +43,15 @@ export default class Matrix extends React.Component{
           lg : [
           {i: 'a', x: 0, y: 0, w: 1, h: 1},
           {i: 'b', x: 1, y: 0, w: 1, h: 1},
-          {i: 'c', x: 4, y: 0, w: 1, h: 1}
+          {i: 'c', x: 2, y: 0, w: 1, h: 1}
           ]
         };
         return (
         <div id="Matrix">        
           <ResponsiveGridLayout
             layouts={layouts}
+            cols={this.default.cols}
+            breakpoints={this.default.breakpoints}
             rowHeight={this.state.rowHeight}
             onBreakpointChange={this.onBreakPointChange}
             verticalCompact={false}
