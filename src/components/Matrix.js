24e6f8cde68a9e714f38fbf733e6b78c29b60a3d
Badr Youbi Idrissi <badryoubiidrissi@gmail.com>
2018-07-25 11:18:30 +0900

    Matrix component resizes or moves tasks

--- src/components/Matrix.js
+++ src/components/Matrix.js
@@ -1,6 +1,9 @@
 import React from "react";
 import {Responsive, WidthProvider} from "react-grid-layout";
 import {connect} from "react-redux";
+import PostIt from "./PostIt";
+import {moveTask, resizeTask} from "../actions";
+import {getMost, getMax} from "../taskUtils";
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
 class Matrix extends React.Component{
@@ -10,6 +13,7 @@ class Matrix extends React.Component{
         breakpoints: {lg: 1800*0.70, md: 1200*0.7, sm: 900*0.9, xs: 600*0.95},
         cols: {lg: 12, md: 10, sm: 6, xs: 4, xxs:2}
       }
+
       this.firstItem = {
         task : null,
         ref : React.createRef()
@@ -20,6 +24,8 @@ class Matrix extends React.Component{
       }
       this.setItemWidth = this.setItemWidth.bind(this);
       this.onLayoutChange = this.onLayoutChange.bind(this);
+      this.onDragStop = this.onDragStop.bind(this);
+      this.onResizeStop = this.onResizeStop.bind(this);
     }
 
     setRowHeight = () => {
@@ -69,15 +75,52 @@ class Matrix extends React.Component{
     }
 
     generatePostIts(){
-      return this.props.tasks.map((task, i) => 
-      <div key={task.id} className="PostIt">
-        <p className="description">
-          {task.name}
-        </p>
+      return this.props.tasks.map(function(task, i){
+        /*This is for having the ref of the first task displayed
+         (in order to calculate width of grid element) */
+        var ref=null;
+        if(i===0){
+          ref = this.firstItem.ref;
+          this.firstItem.task = task;
+        }
+        return (
+          <div key={task.id} ref={ref}>
+            <PostIt task={task}/>
       </div>
     );
+      }.bind(this));
+    }
+    onResize(layout, oldItem, newItem, placeholder){
+      var w = newItem.w;
+      var h = newItem.h;
+      if(w > 2)
+        w = 2;
+      if(h > 2)
+        h = 2;
+      if((w === 2 && h === 1) 
+      || (w === 1 && h === 2) 
+      || (w === 1 && h === 1)){
+        w=1;
+        h=1;
+    }
+      else if(w === 2 && h === 2){
+        w=2;
+        h=2;
+      }
+
+      newItem.w=w;
+      placeholder.w=w;
+      newItem.h=h;
+      placeholder.h=h;
+    }
+    onResizeStop(layout, oldItem, newItem){
+      this.props.resizeTask(newItem.i, newItem.w, newItem.h);
+      console.log(this.props.tasks);
     }
 
+    onDragStop(layout, oldItem, newItem){
+      this.props.moveTask(newItem.i, newItem.y, newItem.x);
+    }
     render() {
         return (
         <div id="Matrix">
@@ -94,7 +137,9 @@ class Matrix extends React.Component{
           rowHeight={this.state.itemWidth}
             onBreakpointChange={this.onBreakPointChange}
             compactType={null}
-            preventCollision={true}>
+          onResize={this.onResize}
+          onResizeStop={this.onResizeStop}
+          onDragStop={this.onDragStop}>
             {this.generatePostIts()}
           </ResponsiveGridLayout>
         </div>
