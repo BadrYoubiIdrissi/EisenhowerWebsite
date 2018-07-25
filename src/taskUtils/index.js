e362a402d8d96dc199674967b29ce40d5066b43a
Badr Youbi Idrissi <badryoubiidrissi@gmail.com>
2018-07-25 13:53:04 +0900

    added task utils

---
+++ src/taskUtils/index.js
@@ -0,0 +1,15 @@
+export function getMost(tasks, key, min = 0) {
+  var max = min;
+  var maxTask = null;
+  tasks.forEach(task => {
+    if (task[key] >= max) {
+      max = task[key];
+      maxTask = task;
+    }
+  });
+  return maxTask;
+}
+
+export function getMax(tasks, key, min=0){
+  return getMost(tasks, key, min)[key];
+}
