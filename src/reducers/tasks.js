454954115a29923913832a9193ba00217f9eb242
Badr Youbi Idrissi <badryoubiidrissi@gmail.com>
2018-07-25 13:52:22 +0900

    changed id to string in default tasks

--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -1,5 +1,6 @@
-import {actions} from "../actions";
-import {categories} from "../constants";
+import { actions } from "../actions";
+import { categories } from "../constants";
+import { getMax } from "../taskUtils";
 
 export function tasks(state, action) {
   var tasks = state ? state.slice() : null;
@@ -7,17 +8,17 @@ export function tasks(state, action) {
   switch (action.type) {
     case actions.ADD_TASK:
       var maxUrg = getMax(tasks, "urgence");
-            tasks.push({
+      tasks.push({
         id: tasks.length + 1,
-                name: action.task.content,
-                description: "",
-                category: action.task.category,
-                width: 2,
-                height: 2,
-                urgence: maxUrg + 2,
-                importance: 0
-            });
-            return tasks;
+        name: action.task.content,
+        description: "",
+        category: action.task.category,
+        width: 2,
+        height: 2,
+        urgence: maxUrg + 2,
+        importance: 0
+      });
+      return tasks;
     case actions.MOVE_TASK:
       task = tasks.find(task => (task.id === action.task.id));
       task.urgence = action.task.urgence;
@@ -28,44 +29,50 @@ export function tasks(state, action) {
       task.width = action.task.width;
       task.height = action.task.height;
       return tasks;
-        default:
-            var defaultTasks = [{
-                id: 1,
-                name: "Urgent and Important",
-                description: "This is a sample urgent and important task",
-                category: categories.URGENT_IMPORTANT,
-                importance: 2,
-                urgence: 0,
-                width: 2,
-                height: 2
-            },{
-                id: 2,
-                name: "Not Urgent and Important",
-                description: "Not so urgent but still important!",
-                category: categories.N_URGENT_IMPORTANT,
-                importance: 2,
-                urgence: 2,
-                width: 2,
-                height: 2
-            },{
-                id: 3,
-                name: "Urgent and Not Important",
-                description: "Wouldn't you like to delegate this?",
-                category: categories.URGENT_N_IMPORTANT,
-                importance: 0,
-                urgence: 0,
-                width: 2,
-                height: 2
-            },{
-                id: 4,
-                name: "Not Urgent and Not Important",
-                description: "Oh come ooooon! There's plenty of time to procrastinate",
-                category: categories.N_URGENT_IMPORTANT,
-                importance: 0,
-                urgence: 2,
-                width: 2,
-                height: 2
-            }];
-            return defaultTasks;
-    }
+    default:
+      var defaultTasks = [
+        {
+          id: "1",
+          name: "Urgent and Important",
+          description: "This is a sample urgent and important task",
+          category: categories.URGENT_IMPORTANT,
+          importance: 2,
+          urgence: 2,
+          width: 2,
+          height: 2
+        },
+        {
+          id: "2",
+          name: "Not Urgent and Important",
+          description: "Not so urgent but still important!",
+          category: categories.N_URGENT_IMPORTANT,
+          importance: 2,
+          urgence: 0,
+          width: 2,
+          height: 2
+        },
+        {
+          id: "3",
+          name: "Urgent and Not Important",
+          description: "Wouldn't you like to delegate this?",
+          category: categories.URGENT_N_IMPORTANT,
+          importance: 0,
+          urgence: 2,
+          width: 2,
+          height: 2
+        },
+        {
+          id: "4",
+          name: "Not Urgent and Not Important",
+          description:
+            "Oh come ooooon! There's plenty of time to procrastinate",
+          category: categories.N_URGENT_N_IMPORTANT,
+          importance: 0,
+          urgence: 0,
+          width: 2,
+          height: 2
+        }
+      ];
+      return defaultTasks;
+  }
 }
