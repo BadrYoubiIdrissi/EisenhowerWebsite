f06474d922f4d47f22b771e9582754a4d5dee832
Badr Youbi Idrissi <badryoubiidrissi@gmail.com>
2018-07-27 18:24:30 +0900

    Added function to correct collisions in a tasks, limit couple

--- src/reducers/taskUtils.js
+++ src/reducers/taskUtils.js
@@ -14,3 +14,48 @@ export function getMost(tasks, key, min = 0) {
 export function getMax(tasks, key, min=0){
   return getMost(tasks, key, min)[key];
 }
+  return getMost(tasks, key, min)[key];
+}
+
+export function getUrgenceImportance(x, y, category, limit){
+  var origins = getOrigins(limit);
+  return {
+    importance : x - origins[category].importance,
+    urgence: y - origins[category].urgence
+  } 
+}
+
+export function correctCollisions(tasks, limit){
+  tasks.forEach(task => {
+      var modification = false;
+      var collision = tasks.find(t => task.id !== t.id && task.category === t.category
+                    && t.urgence <= task.urgence && task.urgence < t.urgence + t.height
+                    && t.importance <= task.importance && task.importance < t.importance + t.width );
+      if(collision){
+        if (collision.height === 2 && collision.urgence === task.urgence){
+          task.urgence += 2;
+        }
+        else{
+          task.urgence += 1;
+        }
+        modification = true;
+      }
+      if((task.category === categories.N_URGENT_IMPORTANT 
+          || task.category === categories.N_URGENT_N_IMPORTANT)
+          && task.urgence+task.height > limit.urgence){
+        limit.urgence = task.urgence+task.height;
+        modification = true;
+      }
+      if(task.importance+task.width > limit.importance){
+        task.importance = limit.importance - task.width;
+        modification = true
+      }
+      if(task.importance+task.width > limit.maxWidth){
+        task.importance = limit.maxWidth - task.width;
+        modification = true
+      }
+      if(modification)
+        correctCollisions(tasks, limit);
+  })
+}
+
