fb93ab565885e94f7b57d3ec24dc9d9c33f458c1
Badr Youbi Idrissi <badryoubiidrissi@gmail.com>
2018-07-30 22:12:44 +0200

    correcting collisions after adjusting limits

--- src/reducers/taskUtils/adjustLimit.js
+++ src/reducers/taskUtils/adjustLimit.js
@@ -1,9 +1,11 @@
 import {categories} from "../../constants";
 import {getMost} from "./getters";
+import {correctCollisions} from "./collision"; 
 
 export default function adjustLimit(tasks,limit){
     const task = getMost(tasks.filter((task) => task.category === categories.N_URGENT_N_IMPORTANT
                                              || task.category === categories.N_URGENT_N_IMPORTANT)
                         ,"urgence");
     limit.urgence= task ? task.urgence+task.height : 2;
+    correctCollisions(tasks, limit);
 }
