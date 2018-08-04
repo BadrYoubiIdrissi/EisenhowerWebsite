7ab809c9dad17d3c66855a6d069282be4b743b82
Badr Youbi Idrissi <badryoubiidrissi@gmail.com>
2018-08-05 01:03:01 +0900

    Delegated user mangement to main reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,13 @@ import {combineReducers} from "redux";
 import limit from "./limit";
 import tasks from "./tasks";
 import breakpoint from "./breakpoint";
+import user from "./user";
+import {reducer as notifications} from "react-notification-system-redux";
 
 export default combineReducers({
     limit,
     tasks,
-    breakpoint
+    breakpoint,
+    notifications,
+    user
 })
-
