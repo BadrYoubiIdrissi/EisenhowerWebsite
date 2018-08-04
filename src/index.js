8130000397a74a9dd54cf28ac295e2678cafc35e
Badr Youbi Idrissi <badryoubiidrissi@gmail.com>
2018-08-05 01:01:53 +0900

    Created main reducer to reset state (for user change actions)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./components/App.js";
 import {Provider} from "react-redux";
 import {createStore, applyMiddleware, compose} from "redux";
+import actions from "./actions";
 import layoutMiddleware from "./middleware/layout";
 import serverMiddleware from "./middleware/server";
 import rootReducer from "./reducers";
@@ -10,13 +11,20 @@ import "./styles/css/index.css";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-var store = createStore(rootReducer,
+var mainReducer = (state = {}, action) => 
+                        action.type === actions.HYDRATE ? action.payload : rootReducer(state, action);
+
+var store = createStore(mainReducer,
     composeEnhancers(applyMiddleware(serverMiddleware,layoutMiddleware))
 );
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById("root")
-);
+store.dispatch({type : actions.HYDRATE, callback: onDone});
+
+function onDone(){
+    ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+        document.getElementById("root")
+    );
+}
